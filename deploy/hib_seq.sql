a60a2d956e8aae939facf2fc12bb7629694d1791
Frank Rodriguez <frank0631@gmail.com>
2018-10-05 05:34:25 +0000

    insert second value into hib_seq table

--- deploy/hib_seq.sql
+++ deploy/hib_seq.sql
@@ -3,9 +3,10 @@
 BEGIN;
 
      create table IF NOT EXISTS hibernate_sequence (
-       next_val bigint
+       next_val bigint(20)
     );
     
     insert into hibernate_sequence (next_val) values (0);
+    insert into hibernate_sequence (next_val) values (1);
 
 COMMIT;
